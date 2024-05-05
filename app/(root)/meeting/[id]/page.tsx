efa19ece1c29e422e5a62cc0e51e4c7c7c11baf1
Jaideep Yogish <92587567+jaideep-y@users.noreply.github.com>
2024-05-05 11:23:31 +0530

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -28,7 +28,7 @@ const MeetingPage = () => {
   // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
   const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  if (notAllowed) return 
 
   return (
     <main className="h-screen w-full">
